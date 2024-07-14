c9335a9e0a5a566628320e671cf198c052385096
nandoys <grnandoy@gmail.com>
2024-07-14 15:54:38 +0100

    Post controller: Change return to findAll method

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('post')
 export class PostController {
   @Get()
-  findAll(): string {
-    return 'Hello post';
+  findAll(): any[] {
+    return [];
   }
 }
